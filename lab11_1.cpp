475d9a2f3106577acb4b556a9ce9068231a2eaa0
Unknown <jantakan27b@gmail.com>
2018-02-05 18:03:04 +0700

    ทำข้อ 9-1 แล้วค่ะ

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -8,10 +8,16 @@ class Student
 private:
     int id;
     // เพิ่มเอาเอง
+    string name;
+    int age;
+    double height;
 public:
     Student(int i, string n, int a, double h)
     {
         id = i;
+        name = n;
+        age = a;
+        height = h;
 
     }
 
@@ -19,11 +25,51 @@ public:
     {
         return id;
     }
+    string getName()
+    {
+       return name;
+    }
+    int getage()
+    {
+         return age;
+    }
+    double getheight()
+    {
+        return height;
+    }
+
     // เพิ่มเอาเอง
 };
 
 int main ()
 {
+    Student Noctis(36548, "Noctis", 20, 176);
+    Student Ignis(36549, "Ignis", 22, 183);
+    Student Gladiolus(36550, "Gladiolus", 23, 198);
+    Student Prompto(36551, "Prompto", 20, 173);
+
+    cout << "Id : " << Noctis.getId() << endl;
+    cout << "Name : " << Noctis.getName() << endl;
+    cout << "age : " << Noctis.getage() << endl;
+    cout << "height : " << Noctis.getheight() << endl;
+
+    cout << "Id : " << Ignis.getId() << endl;
+    cout << "Name : " << Ignis.getName() << endl;
+    cout << "age" << Ignis.getage() << endl;
+    cout << "height : " << Ignis.getheight() << endl;
+
+    cout << "Id : " <<  Gladiolus.getId() << endl;
+    cout << "Name : " <<  Gladiolus.getName() << endl;
+    cout << "age : " <<  Gladiolus.getage() << endl;
+    cout << "height: " <<  Gladiolus.getheight() << endl;
+
+    cout << "Id : " << Prompto.getId() << endl;
+    cout << "Name : " << Prompto.getName() << endl;
+    cout << "age: " << Prompto.getage() << endl;
+    cout << "height : " << Prompto.getheight() << endl;
+
+
+
 
     return 0;
 }
